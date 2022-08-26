e66079d4fb9558735d901d30c7478698e1fe6cb3
johdavid <walletpilot55@gmail.com>
2022-08-26 21:12:40 +0100

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,23 +7,64 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "GANGSHARK";
+const description = "The GANGSHARK is an NFT  collection of 3,500 uniquely generated bored gang shark collectibles that exist on the polygon blockchain.Each Shark is a member of the Shiver Gang of Sharks feared and rule the entire ocean and its wonderful precious treasure and peer. Shiver gangs are not Mafias: they're a lot more peaceful, but also way more cold, ruth. Chilly Shark Gang Club weaves together art, design, marketing, merchandising and a NFT-token to create a compelling experience for fans and collectors alike";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 3000,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Sharks" },
+      { name: "Attire" },
+      { name: "Mouth" },
+      { name: "Eye" },
+      { name: "EyeGlasses" },
+      { name: "Hair Cap" },
     ],
-  },
+  },{
+    growEditionSizeTo: 3050,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Sharks" },
+      { name: "Attire" },
+      { name: "Mouth" },
+      { name: "Eye Laser" },
+      { name: "CL" },
+    ],
+  },{
+    growEditionSizeTo: 3400,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Sharks" },
+      { name: "Mouth" },
+      { name: "Eye" },
+      { name: "EyeGlasses" },
+      { name: "L Attire" },
+      { name: "Hair Cap" },
+    ],
+  },{
+    growEditionSizeTo: 3450,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Sharks" },
+      { name: "L Attire" },
+      { name: "Mouth" },
+      { name: "Eye Laser" },
+      { name: "CL" },
+    ],
+  },{
+    growEditionSizeTo: 3500,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Sharks" },
+      { name: "SL Attire" },
+      { name: "Mouth" },
+      { name: "Eye" },
+      { name: "EyeGlasses" },
+    ],
+  }
 ];
 
 const shuffleLayerConfigurations = true;
@@ -31,30 +72,30 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 400,
+  height: 400,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://NFTCOMBO.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
 
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
-const LIMIT = 2; // Your API key rate limit
-const CHAIN = 'rinkeby'; // only rinkeby, polygon, or ethereum
+const LIMIT = 3; // Your API key rate limit
+const CHAIN = 'polygon'; // only rinkeby, polygon, or ethereum
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'GANGSHARK';
+const CONTRACT_SYMBOL = 'GSN';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xb2a5d4467280cd5425A57D2edc7dD8966b0f335c';
+const TREASURY_ADDRESS = '0xb2a5d4467280cd5425A57D2edc7dD8966b0f335c';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 1.5; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
@@ -62,8 +103,8 @@ const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required.
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xb2a5d4467280cd5425A57D2edc7dD8966b0f335c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
@@ -74,8 +115,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "nft collectables preview comming soon, mint randomly, hold, and wait for the perview day!!"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreidpueep4hjtit54sj46um5psojj5tshtu22t3wnr54geepikow3di"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
@@ -94,11 +135,11 @@ try {
 const solanaMetadata = {
   symbol: "YC",
   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
+  external_url: "https://nftcombo.xyz",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
+      address: "561tF66j2wMNMRaQeKyrfhfkR2zV2eyZZQGuDitYztWF",
+      share: 1000,
     },
   ],
 };
