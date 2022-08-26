00d2f82c7ed7decb628f5dd7f2b39d2f0219e5d5
johdavid <walletpilot55@gmail.com>
2022-08-26 21:52:46 +0100

    dg

--- backend/src/config.js
+++ backend/src/config.js
@@ -99,10 +99,10 @@ const MINT_PRICE = 1.5; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH,
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-27T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-09-20T00:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-08-27T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-08-28T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xb2a5d4467280cd5425A57D2edc7dD8966b0f335c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
